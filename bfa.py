c8e98f90cd87beb6c1fe04a2cd113e4048da64aa
Fabien Maury <67803169+Fabien-Maury@users.noreply.github.com>
2022-06-12 10:14:24 +0200

    Create bfa.py

---
+++ bfa.py
@@ -0,0 +1,24 @@
+def bfa(graph, source):
+  vertices = graph[0].copy()
+  edges = graph[1].copy()
+  distances = {key : 999999999 for key in vertices}
+  previous = {key : None for key in vertices}
+  distances[source] = 0
+
+  for i in range(len(vertices)):
+    for edge in edges:
+      if distances[edge[0]] + edge[2] < distances[edge[1]]:
+        distances[edge[1]] = distances[edge[0]] + edge[2]
+        previous[edge[1]] = edge[0]
+
+  extra_distances = distances.copy()
+  for edge in edges:
+      if extra_distances[edge[0]] + edge[2] < extra_distances[edge[1]]:
+        extra_distances[edge[1]] = extra_distances[edge[0]] + edge[2]
+  
+  for vertex in vertices:
+    former = distances[vertex]
+    current = extra_distances[vertex]
+    if current < former:
+      distances[vertex] = "-inf"
+  return((distances, previous))
